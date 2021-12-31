d5d4550021994d9c35c272df9388f961324e979a
ProgOleg <olegbikov1212@gmail.com>
2021-12-31 18:12:44 +0200

    remove TON

--- utils/constant.py
+++ utils/constant.py
@@ -16,7 +16,7 @@ class Constants:
     BCH = "bch"
     XLM = "xlm"
     DASH = "dash"
-    TON = "ton"
+    # TON = "ton"
     SHIB = "shib"
     DOGE = "doge"
 
@@ -28,7 +28,7 @@ class Constants:
     MEMO_COINS = {XRP, XLM}
     CRYPTO_KOINS = {
         BTC, ETH, LTC, XRP, TRX, USDT_TRC20, USDT_ERC20,
-        BCH, XLM, DASH, TON, SHIB, DOGE, USDC
+        BCH, XLM, DASH, SHIB, DOGE, USDC
     }
     ALL_COINS = CRYPTO_KOINS.union(FIAT)
 
@@ -43,14 +43,14 @@ class Constants:
         ETH: {UAH, BTC, USDT_TRC20, USDT_ERC20, RUB},
         LTC: {UAH, USDT_TRC20, USDT_ERC20},
         XRP: {UAH, USDT_TRC20, USDT_ERC20, RUB},
-        USDT_TRC20: {UAH, BTC, ETH, RUB, USD, XRP, LTC, TRX, USDC, BCH, XLM, DASH, TON, SHIB, DOGE},
-        USDT_ERC20: {UAH, BTC, ETH, RUB, USD, XRP, LTC, TRX, USDC, BCH, XLM, DASH, TON, SHIB, DOGE},
+        USDT_TRC20: {UAH, BTC, ETH, RUB, USD, XRP, LTC, TRX, USDC, BCH, XLM, DASH, SHIB, DOGE},
+        USDT_ERC20: {UAH, BTC, ETH, RUB, USD, XRP, LTC, TRX, USDC, BCH, XLM, DASH, SHIB, DOGE},
         TRX: {UAH, USDT_TRC20, USDT_ERC20},
         USDC: {UAH, BTC, USDT_TRC20, USDT_ERC20},
         BCH: {UAH, USDT_TRC20, USDT_ERC20},
         XLM: {UAH, USDT_TRC20, USDT_ERC20},
         DASH: {UAH, USDT_TRC20, USDT_ERC20},
-        TON: {USDT_TRC20, USDT_ERC20},
+        # TON: {USDT_TRC20, USDT_ERC20},
         SHIB: {UAH, USDT_TRC20, USDT_ERC20},
         DOGE: {UAH, USDT_TRC20, USDT_ERC20}
     }
@@ -58,6 +58,6 @@ class Constants:
     VARIATION_OF_COINS = {
         "btc,usdt", "btc,usdc", "btc,usd", "btc,uah", "btc,rub", "eth,btc", "eth,usdt", "eth,uah", "eth,rub",
         "ltc,usdt", "ltc,uah", "xrp,usdt", "xrp,uah", "xrp,rub", "bch,usdt", "xlm,usdt", "shib,usdt", "usdt,usdc",
-        "usdt,usd", "doge,usdt", "ton,usdt", "trx,usdt", "usdt,uah", "usdt,rub", "dash,usdt", "trx,uah", "usdc,uah",
+        "usdt,usd", "doge,usdt", "trx,usdt", "usdt,uah", "usdt,rub", "dash,usdt", "trx,uah", "usdc,uah",
         "bch,uah", "xlm,uah", "dash,uah", "shib,uah", "doge,uah"
     }
